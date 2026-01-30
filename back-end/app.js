ff1304868c61f5aef891d1f4f68b42f2d8c096fe
Ahmed Asaad <ahmedasaadarn@gmail.com>
2026-01-31 02:53:16 +0400

    Setting up the back-end

---
+++ back-end/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
