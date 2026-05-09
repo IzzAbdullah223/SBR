dd7867011761b977edb7e4f686d7d3d318536006
Ahmed Asaad <ahmedasaadarn@gmail.com>
2026-05-10 03:27:47 +0400

    changed the  map api provider from OpenstreetMap to CartoDB Voyager

--- front-end/src/utils/constants.js
+++ front-end/src/utils/constants.js
@@ -4,10 +4,8 @@ export const MAP_CONFIG = {
   DEFAULT_ZOOM: 11,
   MIN_ZOOM: 10,
   MAX_ZOOM: 18,
-  TILE_LAYER: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-
-  ATTRIBUTION:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  TILE_LAYER: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+ATTRIBUTION: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 };
 
 export const ICON_COLORS = {
